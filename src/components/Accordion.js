9f191cfb61e5a6bcc6ba56ab6481e75fbb7b01ec
Marion SALAUN <marion.salauen.001@student.uni.lu>
2024-07-29 21:09:06 +0200

    fix: scrolldown done, navbar fixed, hamburger fixed

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,68 +1,89 @@
 import gsap from 'gsap';
-import React, {  useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/accordion.css';
 
 function useEffectOnce(effect) {
-    const effectFn = useRef(effect)
-    const destroyFn = useRef()
-    const effectCalled = useRef(false)
-    const rendered = useRef(false)
-    const [, refresh] = useState(0)
-  
-    if (effectCalled.current) {
-      rendered.current = true
+  const effectFn = useRef(effect);
+  const destroyFn = useRef();
+  const effectCalled = useRef(false);
+  const rendered = useRef(false);
+  const [, refresh] = useState(0);
+
+  if (effectCalled.current) {
+    rendered.current = true;
+  }
+
+  useEffect(() => {
+    if (!effectCalled.current) {
+      destroyFn.current = effectFn.current();
+      effectCalled.current = true;
     }
-  
-    useEffect(() => {
-      if (!effectCalled.current) {
-        destroyFn.current = effectFn.current()
-        effectCalled.current = true
-      }
-  
-      refresh(1)
-  
-      return () => {
-        if (rendered.current === false) return
-        if (destroyFn.current) destroyFn.current()
-      }
-    }, [])
+
+    refresh(1);
+
+    return () => {
+      if (rendered.current === false) return;
+      if (destroyFn.current) destroyFn.current();
+    };
+  }, []);
 }
 
+export default function Accordion() {
+  useEffectOnce(() => {
+    const items = document.querySelectorAll('.item');
 
-export default function Accordion(){
+    const expand = (item, i) => {
+      items.forEach((it, ind) => {
+        if (i === ind) return;
+        it.clicked = false;
+      });
+      gsap.to(items, {
+        width: item.clicked ? '15vw' : '8vw',
+        duration: 2,
+        ease: 'elastic(0.5, .9)',
+      });
+      item.clicked = !item.clicked;
+      gsap.to(item, {
+        width: item.clicked ? '48vw' : '15vw',
+        duration: 2.5,
+        ease: 'elastic(0.5, .9)',
+      });
+    };
+    items.forEach((item, i) => {
+      item.clicked = false;
+      item.addEventListener('click', () => expand(item, i));
+    });
+  });
 
-    useEffectOnce(() => {
-        const items = document.querySelectorAll('.item')
-
-        const expand = (item, i) => {
-            items.forEach((it, ind) => {
-                if (i === ind) return
-                it.clicked = false
-            })
-            gsap.to(items, {
-                width: item.clicked ? '15vw' : '8vw',
-                duration: 2,
-                ease: 'elastic(0.5, .9)'
-            })
-            item.clicked = !item.clicked
-            gsap.to(item, {
-                width: item.clicked ? '48vw' : '15vw',
-                duration: 2.5,
-                ease: 'elastic(0.5, .9)'
-            })
-        }
-        items.forEach((item, i) => {
-            item.clicked = false
-            item.addEventListener('click', () => expand(item, i))
-        })
-       
-    });   
-
-    return (
-        <div className="group">
-            <div className="item" style={{"backgroundImage": "url(images/Blob.png)"}}></div>            
-            <div className="item" style={{"backgroundImage": "url(images/GenderPayGap.png)"}}></div>
-            <div className="item" style={{"backgroundImage": "url(images/Circurement.png)"}}></div>
-        </div>
-    )
+  return (
+    <div className="group">
+      <div
+        className="item"
+        style={{ backgroundImage: 'url(images/Blob.png)' }}
+      ></div>
+      {/* <Link to="https://github.com/salaun-marion/GenderPayGap"> */}
+      <div
+        className="item"
+        style={{ backgroundImage: 'url(images/GenderPayGap.png)' }}
+      ></div>
+      {/* </Link> */}
+      <div
+        className="item"
+        style={{
+          backgroundImage: 'url(images/Circurement.png)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      ></div>
+      <div
+        className="item"
+        style={{
+          backgroundImage: 'url(images/board.png)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      ></div>
+    </div>
+  );
 }
